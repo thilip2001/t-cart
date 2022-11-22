e45420e6bd8b49dcedb72bd7b20b7e9ad64f74d3
Thiliban <thilip2017@gmail.com>
2022-11-23 02:05:36 +0530

    fetch data from api

--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useReducer, useEffect } from "react";
+import React, { useContext, useReducer, useEffect } from "react";
 import cartItems from "./data";
 import reducer from "./reducer";
 
@@ -27,6 +27,17 @@ const AppProvider = ({ children }) => {
   const decrease = (id) => {
     dispatch({ type: "DECREASE", payload: id });
   };
+
+  const fetchData = async () => {
+    dispatch({ type: "LOADING" });
+    const response = await fetch(url);
+    const cart = await response.json();
+    dispatch({ type: "DISPLAY_ITEMS", payload: cart });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   useEffect(() => {
     dispatch({ type: "GET_TOTALS" });
   }, [state.cart]);
